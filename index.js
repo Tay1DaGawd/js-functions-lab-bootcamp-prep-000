c505aeceaefccda3f7d88db95c4b40bffcc5267c
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-21 09:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function happyHolidays() {
-  return ('Happy Holidays!')
+  return ("Happy Holidays!")
 }
-function happyHolidaysTo(name) {
+function happyHolidaysTo(name) {"
   return ("Happy Holidays, ${name}!")
 }
 function happyHolidaysTo(holiday, name) {
   return ("Happy ${holiday}, ${name}!")
 }
+

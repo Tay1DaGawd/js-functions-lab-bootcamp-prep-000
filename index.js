ea94eb6e0d682631896292943d9bb5ba3071e474
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-21 10:24:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 function happyHolidays() {
   return ("Happy holidays!")
 }
-var name = "you"
+
 function happyHolidaysTo(name) {
   return ('Happy holidays, ${name}!')
 }
-var name = "you"
-var holiday = "Independence Day"
+
 function happyHolidayTo(holiday, name) {
   return ('Happy ${holiday}, ${name}!')
 }
-var days = 20
-var holiday = "Mothers Day"
+
 function holidayCountdown(holiday, days) {
   return ("It\'s ${days} days until ${holiday}!")
 }
